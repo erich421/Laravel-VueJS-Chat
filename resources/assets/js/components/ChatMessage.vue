59f778633411a6fa77b3283d9c9b70dd8192f4cd
Eric Hendershott <acseric@yahoo.com>
2017-07-20 20:25:07 -0500

    Scaffold Vue components

---
+++ resources/assets/js/components/ChatMessage.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div>
+    <p>{{ message }}</p>
+    <small>{{ user }}</small>
+  </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        message: "This is some message text",
+        user: "John Doe"
+      }    
+    }
+}
+</script>
+
+<style lang="css">
+</style>
